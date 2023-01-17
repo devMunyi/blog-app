eb0b217a341467ce37c12c3fccd59307e4d94910
devMunyi <samunyi90@gmail.com>
2023-01-17 15:45:32 +0300

    Add rubocop setup and fix flagged errors

--- db/migrate/20230117120636_create_comments.rb
+++ db/migrate/20230117120636_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :author, null: false, foreign_key: {to_table: :users}
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.string :post_references
 
       t.timestamps

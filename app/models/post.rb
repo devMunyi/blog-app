1a4588b5c5ac0f89ac4aac28c94494c3598f4f82
devMunyi <samunyi90@gmail.com>
2023-01-19 14:36:33 +0300

    Add validations to User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ApplicationRecord
+  # associations
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  #validations
+  validates :title, presence: true, length: {maximum: 250}
+  validates :comments_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :likes_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
 
   after_save :increment_author_posts_counter
 

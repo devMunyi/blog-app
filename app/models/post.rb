611bc45d4047e244b2c6b026f8b11953ee8b7810
devMunyi <samunyi90@gmail.com>
2023-01-25 18:14:34 +0300

    Update PostsController to return user and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@ class Post < ApplicationRecord
   after_save :increment_author_posts_counter
 
   def five_recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.order(id: :desc).limit(5)
   end
 
   private
